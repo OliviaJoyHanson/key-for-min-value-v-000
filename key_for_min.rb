8a9af3a9552c093950e2725ea6f0be00500b95f7
Olivia Hanson <olivia.joy.hanson@gmail.com>
2020-05-12 12:12:56 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(hash)
-
+  array_of_values = hash.select {|key, value| value}
+  array_of_values.select do |value|
+    if value.index == 0
+      value = a
+    elsif value < a 
+      value = a
+    elsif value > a
+      a
+    end
+  end
+  
 end
